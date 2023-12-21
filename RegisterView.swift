76f1b3d6a19c049e6ac3c3f5dbfab15505280598
sepehr fazely <119894836+sepfazeli@users.noreply.github.com>
2023-12-20 23:58:39 -0500

    Add files via upload

---
+++ RegisterView.swift
@@ -0,0 +1,53 @@
+//
+//  RegisterView.swift
+//  ToDoList
+//
+//  Created by Afraz Siddiqui on 4/30/23.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @StateObject var viewModel = RegisterViewViewModel()
+
+    var body: some View {
+        VStack {
+            // Header
+            HeaderView(title: "Register",
+                       subtitle: "Start organizing todos",
+                       angle: -15,
+                       background: .orange)
+
+            Form {
+                TextField("Full Name", text: $viewModel.name)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocorrectionDisabled()
+                
+                TextField("Email Address", text: $viewModel.email)
+                    .textFieldStyle(DefaultTextFieldStyle())
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+
+                SecureField("Password", text: $viewModel.password)
+                    .textFieldStyle(DefaultTextFieldStyle())
+
+                TLButton(
+                    title: "Create Account",
+                    background: .green
+                ) {
+                    viewModel.register()
+                }
+                .padding()
+            }
+            .offset(y: -50)
+
+            Spacer()
+        }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
---
+++ ToDoListView.swift
@@ -0,0 +1,58 @@
+//  ToDoListItemsView.swift
+//  ToDoList
+//
+//  Created by Afraz Siddiqui on 4/30/23.
+//
+
+import FirebaseFirestoreSwift
+import SwiftUI
+
+struct ToDoListView: View {
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
+
+    init(userId: String) {
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/todos"
+        )
+        self._viewModel = StateObject(
+            wrappedValue: ToDoListViewViewModel(userId: userId)
+        )
+    }
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button("Delete") {
+                                viewModel.delete(id: item.id)
+                            }
+                            .tint(.red)
+                        }
+                }
+                .listStyle(PlainListStyle())
+            }
+            .navigationTitle("To Do List")
+            .toolbar {
+                Button {
+                    // Action
+                    viewModel.showingNewItemView = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+
+            }
+            .sheet(isPresented: $viewModel.showingNewItemView) {
+                NewItemView(newItemPresented: $viewModel.showingNewItemView)
+            }
+        }
+    }
+}
+
+struct ToDoListItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListView(userId: "RcNUw88NDCOuzwgBeispLnTTLuv2")
+    }
+}
